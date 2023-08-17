54b840452e8a3c41318892afc79527f5736a979c
edison.estramil <edison.estramil@fing.edu.uy>
2023-08-17 14:30:49 -0300

    auth

--- frontend/example/src/app/app.module.ts
+++ frontend/example/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
--- frontend/example/src/app/pages/login/login.component.ts
+++ frontend/example/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
@@ -10,16 +11,26 @@ export class LoginComponent {
   username : string = "";
   password : string =  "";
   mensaje = ""
+  mostrarDialog = false;
 
-  constructor(private usuarioService:UsuarioService){}
+  constructor(private usuarioService:UsuarioService, private router: Router,private activatedRouter : ActivatedRoute){}
   async authUser(){
     try{
       const result = await this.usuarioService.authenticate(this.username, this.password).toPromise();
-      this.mensaje = "USUARIO CORRECTO"
+      this.router.navigate(['/inicio']); 
       console.log(result);
     }catch(error){
-      this.mensaje = "USUARIO INCORRECTO"
+      this.timerDialog("USUARIO INCORRECTO");
       console.log(error);
     }
   }
+
+  timerDialog(mensaje : string){
+    this.mostrarDialog = true;
+    this.mensaje = mensaje;
+    setTimeout(() => {
+      this.mostrarDialog = false;
+    }, 4000);
+  }
 }
+

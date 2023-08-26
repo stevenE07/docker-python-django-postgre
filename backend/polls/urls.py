386acceb95fc8d13692984d65b460daa129fef50
Agustín Recoba <agustin.recoba@fing.edu.uy>
2023-08-26 19:02:13 -0300

    Admin site levantado

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
+from django.contrib import admin
 
 from knox import views as knox_views
 
 from .views import CreateUserView, LoginView, ManageUserView
 
-app_name = 'core'
+app_name = "core"
 
 urlpatterns = [
-    path('create/', CreateUserView.as_view(), name="create"),
-    path('profile/', ManageUserView.as_view(), name='profile'),
-    path('login/', LoginView.as_view(), name='knox_login'),
-    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
-    path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
+    path("create/", CreateUserView.as_view(), name="create"),
+    path("profile/", ManageUserView.as_view(), name="profile"),
+    path("login/", LoginView.as_view(), name="knox_login"),
+    path("logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
+    path("logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
+    path("admin/", admin.site.urls),
 ]
